9170bbc40bb1f219e5283fb84183467c5c03d423
unknown <nettashani@gmail.com>
2023-10-25 13:38:41 +0300

    minor style

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -3,7 +3,7 @@ import './pages.css';
 
 export const LandingPage = () => {
   return (
-    <div className="home-page-app">
+    <div className="landing-page-app">
       <div className="home-page-landing">
         <Link to="home" className="app-link">
           <div className="landing-page-title">מְשַׂחֲקִים וְלוֹמְדִים</div>
