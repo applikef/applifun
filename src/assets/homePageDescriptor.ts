3afaa6a4506e7ee83c9351f942295fe78ce81cf5
netta <nettashani@gmail.com>
2023-11-01 16:26:44 +0200

    update home page layout to group games

--- src/assets/homePageDescriptor.ts
+++ src/assets/homePageDescriptor.ts
@@ -3,7 +3,7 @@ export type PointType = {
   y: string;
 }
 
-export type HomePageDescriptorType = {
+export type HomePageItemType = {
   id: string;
   label: string;
   path: string;
@@ -11,26 +11,47 @@ export type HomePageDescriptorType = {
   height?: number;
 }
 
-export const homePageDescriptor: HomePageDescriptorType[] = [
+export type HomePageSectionType = {
+  title?: string;
+  items: HomePageItemType[];
+}
+
+export const homePageDescriptor: HomePageSectionType[] = [
   {
-    id: "colorMatch",
-    label: "מוֹצְאִים תְּמוּנוֹת בְּצֶבַע",
-    path: "/launch?gameId=colorMatch",
-    media: "resources/images/colorדSplash.png",
-    height: 200
+    items: [
+      {
+        id: "colorMatch",
+        label: "מוֹצְאִים תְּמוּנוֹת בְּצֶבַע",
+        path: "/launch?gameId=colorMatch",
+        media: "resources/images/colors-splash.png",
+        height: 100
+      },
+      {
+        id: "numberMatch",
+        label: "סוֹפְרִים דְּבָרִים",
+        path: "/launch?gameId=numberMatch",
+        media: "resources/images/numbers-splash.png",
+        height: 100
+      },
+      {
+        id: "letterMatch",
+        label: "מוֹצְאִים אוֹתִיּוֹת",
+        path: "/launch?gameId=letterMatch",
+        media: "resources/images/letters-splash.png",
+        height: 100
+      }
+    ]
   },
   {
-    id: "numberMatch",
-    label: "סוֹפְרִים דְּבָרִים",
-    path: "/launch?gameId=numberMatch",
-    media: "resources/images/numbersSplash.png",
-    height: 200
-  },
-  {
-    id: "letterMatch",
-    label: "מוֹצְאִים אוֹתִיּוֹת",
-    path: "/launch?gameId=letterMatch",
-    media: "resources/images/lettersSplash.png",
-    height: 200
+    "title": "אֲנִי עוֹשֶׂה",
+    "items": [
+      {
+        id: "washHands",
+        label: "שׁוֹטֵף יָדַיִם",
+        path: "/launch?gameId=washHands",
+        media: "resources/images/wash-hands.jpg",
+        height: 100
+      }
+    ]
   }
 ];
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,7 +1,7 @@
 import { Link } from "react-router-dom";
 
 import { Card } from "../components/shared/Card/Card";
-import { HomePageDescriptorType, homePageDescriptor } from  "../assets/homePageDescriptor";
+import { HomePageItemType, HomePageSectionType, homePageDescriptor } from  "../assets/homePageDescriptor";
 
 import './pages.css';
 
@@ -10,14 +10,22 @@ export const HomePage = () => {
     <div className="home-page-app">
       <div className="home-page-content-area">
         <div className="home-page-title">מְשַׂחֲקִים וְלוֹמְדִים</div>
-          <div className='home-page-games-list' data-walkthrough="app-games-list">
-            {homePageDescriptor.map((game: HomePageDescriptorType,i) => 
-              <Card key={game.id}
-                  content={<Link to={game.path} className="app-link">{game.label}</Link>}
-                  media={game.media ? game.media : undefined}
-                  linkMedia={game.path}
-                  height={game.height ? game.height : undefined}
-              />
+          <div className='home-page-section-list' data-walkthrough="app-games-list">
+            {homePageDescriptor.map((section: HomePageSectionType,i) =>
+              <div className="home-page-games-list" key={i}>
+                {section.title &&
+                  <div className="app-sub-title">{section.title}</div>
+                } 
+                    <div className="line-break"></div>
+                {section.items.map((game: HomePageItemType,i) => 
+                  <Card key={game.id}
+                      content={<Link to={game.path} className="app-link app-default-text">{game.label}</Link>}
+                      media={game.media ? game.media : undefined}
+                      linkMedia={game.path}
+                      height={game.height ? game.height : undefined}
+                  />
+                )}
+              </div>
             )}
           </div>
       </div>
