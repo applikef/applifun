20c86cbb223181797617104ab3503f0ebd4c08ef
netta <nettashani@gmail.com>
2023-11-02 13:03:05 +0200

    hide and show top banner

--- src/components/shared/Banner/Banner.tsx
+++ src/components/shared/Banner/Banner.tsx
@@ -1,30 +1,51 @@
 import { Link } from "react-router-dom";
 
 import './Banner.css'
+import { useState } from "react";
 
 export interface BannerPropsType {
   settings?: Function;
 }
 
 export const Banner = (props: BannerPropsType) => {
+  const [showBanner, setShowBanner] = useState<boolean>(true);
+
   return (
     <>
-      <div className="banner-icon-bar">
-        <div className="banner-right-icon-bar">
-          <Link to="/home">
-            <img src="resources/icons/home128.png" className="banner-icon" 
-              title="עמוד הבית"  alt="עמוד הבית" />
-          </Link>
-          { props.settings &&
-            <img src="resources/icons/settings.png" className="banner-icon" 
-              title="הגדרות משחק"  alt="הגדרות משחק" 
-              onClick={() => props.settings ? props.settings() : undefined}/>
-          }
+      {showBanner &&
+        <div className="banner-icon-bar">
+          <div className="banner-right-icon-bar">
+            <Link to="/home">
+              <img src="resources/icons/home128.png" className="banner-icon" 
+                title="עמוד הבית"  alt="עמוד הבית" />
+            </Link>
+            { props.settings &&
+              <img src="resources/icons/settings.png" className="banner-icon" 
+                title="הגדרות משחק"  alt="הגדרות משחק" 
+                onClick={() => props.settings ? props.settings() : undefined}/>
+            }
+          </div>
+          <div className="banner-left-icon-bar">
+            <div>
+              <img src="resources/icons/speaker.png" className="banner-icon" 
+                  title="השתק"  alt="השתק" />
+            </div>
+            <div>
+              <img src="resources/icons/hide.png" className="banner-icon" 
+                  title="החבא תפריט שליטה"  alt="החבא תפריט שליטה"
+                  onClick={() => setShowBanner(false)} />
+            </div>
+          </div>
         </div>
-        <div className="banner-left-icon-bar">
+      }
+      {showBanner && <hr className="banner-hr"/>}
+      {!showBanner &&
+        <div>
+          <img src="resources/icons/show.png" className="banner-icon" 
+              title="הראה תפריט שליטה"  alt="הראה תפריט שליטה"
+              onClick={() => setShowBanner(true)} />
         </div>
-      </div>
-      <hr className="banner-hr"/>
+      }
     </>
   );
 }
