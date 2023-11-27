50dfe9d978c1fdcbafe1c565a0ee1fe59476ff70
netta <nettashani@gmail.com>
2023-11-27 17:33:36 +0200

    home page layout, issue #9

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -10,6 +10,7 @@ import { LineBreak } from "../components/shared/LineBreak";
 
 export const HomePage = () => {
   const isMobile = DeviceUtil.isMobileDevice();
+  const isSmallScreen = DeviceUtil.isSmallDevice();
 
   const [showSection, setShowSection] = useState<Array<boolean>>(new Array<boolean>(homePageDescriptor.length));
 
@@ -23,29 +24,49 @@ export const HomePage = () => {
     <div className="home-page-app">
       <div className="home-page-content-area">
         <div className="home-page-title">מְשַׂחֲקִים וְלוֹמְדִים</div>
-        <div className="home-page-sub-title app-indent-top-16">בְּחַר סוּג מִשְׂחָק לִרְאוֹת אֶת הַמִּשְׂחָקִים הַקַּיָּמִים</div>
-          <div className='home-page-section-list' data-walkthrough="app-games-list">
-            {homePageDescriptor.map((section: HomePageSectionType,i) =>
-              (!isMobile ||
-                (isMobile && (section.mobile ? section.mobile : true))) &&
-                <div className="home-page-games-list" key={i}>
-                  <div className="app-clickable" onClick={() => updateShowSection(i)}>
-                    {section.title &&
-                      <div className="app-sub-title">{section.title}</div>
-                    } 
-                    <LineBreak />
-                    {section.media &&
-                      <img src={section.media} height={DeviceUtil.imageHeight()} 
-                        alt={section.title} />
-                    } 
-                  </div>
-                </div>
-            )}
-          </div>
+        { isSmallScreen ? 
           <div>
+            <div className="home-page-sub-title app-indent-top-16">בְּחַר סוּג מִשְׂחָק לִרְאוֹת אֶת הַמִּשְׂחָקִים הַקַּיָּמִים</div>
+            <div className='home-page-section-list' data-walkthrough="app-games-list">
+              {homePageDescriptor.map((section: HomePageSectionType,i) =>
+                (!isMobile ||
+                  (isMobile && (section.mobile ? section.mobile : true))) &&
+                  <div className="home-page-games-list" key={i}>
+                    <div className="app-clickable" onClick={() => updateShowSection(i)}>
+                      {section.title &&
+                        <div className="app-sub-title">{section.title}</div>
+                      } 
+                      <LineBreak />
+                      {section.media &&
+                        <img src={section.media} height={DeviceUtil.imageHeight()} 
+                          alt={section.title} />
+                      } 
+                    </div>
+                  </div>
+              )}
+            </div>
+            <div>
+              {homePageDescriptor.map((section: HomePageSectionType,i) =>
+                <div key={i} 
+                  className={`home-page-games-list-items ${showSection[i] ? "app-show-flex" : "app-hide"}`} >
+                  <div className="app-sub-title home-page-games-list-title">{section.title}</div>
+                  {section.items.map((game: HomePageItemType,i) => 
+                    <Card key={game.id}
+                        content={<Link to={game.path} className="app-link app-default-text">{game.label}</Link>}
+                        media={game.media ? game.media : undefined}
+                        linkMedia={game.path}
+                        height={game.height ? game.height : undefined}
+                    />
+                  )}
+                </div>
+              )}
+            </div>
+          </div> 
+        :
+          <div className="home-page-games-list-large-device-list">
             {homePageDescriptor.map((section: HomePageSectionType,i) =>
               <div key={i} 
-                className={`home-page-games-list-items ${showSection[i] ? "app-show-flex" : "app-hide"}`} >
+                className="home-page-games-list-items home-page-games-list-large-device app-show-flex">
                 <div className="app-sub-title home-page-games-list-title">{section.title}</div>
                 {section.items.map((game: HomePageItemType,i) => 
                   <Card key={game.id}
@@ -58,11 +79,12 @@ export const HomePage = () => {
               </div>
             )}
           </div>
-          <div className="app-padding-top-32">
-            <Link to="/launch?gameId=changeRecords" className="app-link-sm">
-              לרשימת כל עדכוני המשחקים
-            </Link> 
-          </div>
+        }  
+      </div>
+      <div>
+        <Link to="/launch?gameId=changeRecords" className="app-link-sm">
+          לרשימת כל עדכוני המשחקים
+        </Link> 
       </div>
     </div>
   );
